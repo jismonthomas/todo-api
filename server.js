4809d5011d647cf2481c427100a1279bdf53efb1
Jismon Thomas <jismonthomas18@gmail.com>
2016-11-08 10:10:54 -0800

    add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: "go to atm",
-  completed: false
-}, {
-  id: 2,
-  description: "get haircut",
-  completed: false
-}, {
-  id: 3,
-  description: "study node/js",
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req, res){
   res.send('Todo api Root');
@@ -38,9 +30,19 @@ app.get('/todos/:id',function(req, res){
     res.status(404).send();
     //res.send("not found");
   }
-  //
 });
 
+app.post('/todos',function(req, res) {
+  var body = req.body;
+
+  body.id = todoNextId;
+  todoNextId++;
+
+  todos.push(body);
+  console.log("description "+body.description);
+
+  res.send(body);
+});
 
 app.listen(PORT, function() {
   console.log("express listening on port"+ PORT);
