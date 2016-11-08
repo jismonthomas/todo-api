6febd32097d1a95fb26f4ce80d681ce06f97335e
Jismon Thomas <jismonthomas18@gmail.com>
2016-11-08 12:20:39 -0800

    added templates

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require("express");
 var bodyParser = require('body-parser');
+var _ = require("underscore");
+var hbs = require('hbs');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 
 app.get('/',function(req, res){
-  res.send('Todo api Root');
+  res.render('home.hbs',{
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to the website",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 //get /todos
@@ -18,22 +27,36 @@ app.get('/todos',function(req, res){
 //get /todos/:id
 
 app.get('/todos/:id',function(req, res){
-  var todoId = req.params.id;
-  var matchedItem;
-  todos.forEach(function(todo){
-    if(todo.id == todoId) {
-      matchedItem = todo.id;
-      res.json(todo);
-    }
-  });
-  if (typeof matchedItem === "undefined") {
-    res.status(404).send();
-    //res.send("not found");
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  // var matchedItem;
+  // todos.forEach(function(todo){
+  //   if(todo.id == todoId) {
+  //     matchedItem = todo.id;
+  //     res.json(todo);
+  //   }
+  // });
+
+  if(matchedTodo) {
+    res.json(matchedTodo);
   }
+  else {
+    res.status(404).send();
+  }
+
+  // if (typeof matchedItem === "undefined") {
+  //   res.status(404).send();
+  //   //res.send("not found");
+  // }
 });
 
 app.post('/todos',function(req, res) {
   var body = req.body;
+
+  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(404).send();
+  }
 
   body.id = todoNextId;
   todoNextId++;
@@ -44,6 +67,12 @@ app.post('/todos',function(req, res) {
   res.send(body);
 });
 
+app.get('/about', function(req, res) {
+  res.render('about.hbs', {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
+});
 app.listen(PORT, function() {
   console.log("express listening on port"+ PORT);
 });
